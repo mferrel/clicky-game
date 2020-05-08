d9fa3631f40e6f4db6ba703256a3c5a30cba6c40
Marena Linda <marena25@protonmail.com>
2020-05-07 22:31:46 -0400

    creates the tiles and counter

--- src/App.js
+++ src/App.js
@@ -1,41 +1,14 @@
-import React, { Component } from "react";
-import FriendCard from "./components/FriendCard";
+import React from "react";
 import Wrapper from "./components/Wrapper";
-import Title from "./components/Title";
-import friends from "./friends.json";
+import Jumbotron from "./components/Jumbotron";
+import "./index.css";
 
-class App extends Component {
-  // Setting this.state.friends to the friends json array
-  state = {
-    friends
-  };
 
-  removeFriend = id => {
-    // Filter this.state.friends for friends with an id not equal to the id being removed
-    const friends = this.state.friends.filter(friend => friend.id !== id);
-    // Set this.state.friends equal to the new friends array
-    this.setState({ friends });
-  };
-
-  // Map over this.state.friends and render a FriendCard component for each friend object
-  render() {
-    return (
-      <Wrapper>
-        <Title>Friends List</Title>
-        {this.state.friends.map(friend => (
-          <FriendCard
-            removeFriend={this.removeFriend}
-            id={friend.id}
-            key={friend.id}
-            name={friend.name}
-            image={friend.image}
-            occupation={friend.occupation}
-            location={friend.location}
-          />
-        ))}
-      </Wrapper>
-    );
-  }
-}
+const App = () => (
+  <div className="container-fluid mainContainer">
+    <Jumbotron />
+    <Wrapper /> 
+  </div>
+);
 
 export default App;
