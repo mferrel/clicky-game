d177ec1f5f26a99c33a0744e4e0d0bc9cb30a6fd
Marena Linda <marena25@protonmail.com>
2020-05-07 22:51:45 -0400

    edits jumbotron colors

--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -4,7 +4,8 @@ import "./style.css";
 const Jumbotron = () => (
   <div className="jumbotron jumbotron-fluid">
     <div className="container text-center">
-      <h1 className="display-4">React Memory Game</h1>
+      <h1 className="display-4">Killing Eve</h1>
+      <h2 className="display-4">Memory Game</h2>
       <p className="lead">Click on a tile to earn points, but don't click on any more than once!</p>
     </div>
   </div>
