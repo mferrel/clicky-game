124f3c0ab91907ccd07929ff9316ec84b9bc07ad
Marena Linda <marena25@protonmail.com>
2020-05-10 21:07:41 -0400

    Updates

---
+++ precache-manifest.1ed4d436d166db58ba8083ad332d036b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5de0042b52bb2d27cbbdc2e888b3143",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "1825f634a6aa1345ea3a",
+    "url": "/clicky-game/static/css/main.5a550980.chunk.css"
+  },
+  {
+    "revision": "b29349011b921a80e2e6",
+    "url": "/clicky-game/static/js/2.ea3a9362.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game/static/js/2.ea3a9362.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1825f634a6aa1345ea3a",
+    "url": "/clicky-game/static/js/main.3b2d1cdf.chunk.js"
+  },
+  {
+    "revision": "b631d7080dcb249ab8e5",
+    "url": "/clicky-game/static/js/runtime-main.ea51dac7.js"
+  }
+]);
